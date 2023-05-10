dfa404f89cbfb568f1ec39e749841036399bf004
kwell <74409210+DmitriyKwelov@users.noreply.github.com>
2023-05-10 21:32:37 +0300

    ghsrtgserdg

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
+const https = require("https");
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const mongoose = require("mongoose");
 const router = require('./router/index')
 const path = require('path')
+const fs = require("fs");
 
 const PORT = process.env.PORT || 5000;
 const app = express()
@@ -24,13 +26,27 @@ app.use(cors({
 }));
 app.use('/api', router);
 
+
 const start = async () => {
     try {
         await mongoose.connect(process.env.DB_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         })
-        app.listen(PORT, () => console.log(`server started on ${PORT} PORT`))
+        https
+            .createServer(
+                // Provide the private and public key to the server by reading each
+                // file's content with the readFileSync() method.
+                {
+                    key: fs.readFileSync("key.pem"),
+                    cert: fs.readFileSync("cert.pem"),
+                },
+                app
+            )
+            .listen(4000, () => {
+                console.log("serever is runing at port 4000");
+            });
+        // app.listen(PORT, () => console.log(`server started on ${PORT} PORT`))
     } catch (e) {
         console.log(e)
     }
